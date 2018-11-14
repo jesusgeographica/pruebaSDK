06f730414eb8c55bad1204448102ae754350bf20
Jesus Victorio <jesus.victorio@geographica.gs>
2018-11-14 14:00:37 +0100

    sdk 0.1.1

---
+++ Example/Tempo-ios-sdk/ViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  Tempo-ios-sdk
+//
+//  Created by Javier Aragón on 10/02/2018.
+//  Copyright (c) 2018 Javier Aragón. All rights reserved.
+//
+
+import UIKit
+import Tempo_ios_sdk
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let mapView = UIMapView.init(frame: view.bounds)
+        view.addSubview(mapView)
+        
+        let geoJson: [String : Any]  = [
+            "type": "FeatureCollection",
+            "features": [
+                [
+                    "type": "Feature",
+                    "properties": nil,
+                    "geometry": [
+                        "type": "Polygon",
+                        "coordinates": [
+                            [
+                                [
+                                    -3.6944103240966797,
+                                    40.44734712965495
+                                ],
+                                [
+                                    -3.694442510604858,
+                                    40.44666945857137
+                                ],
+                                [
+                                    -3.693326711654663,
+                                    40.44662047003545
+                                ],
+                                [
+                                    -3.693380355834961,
+                                    40.44739611766125
+                                ],
+                                [
+                                    -3.6944103240966797,
+                                    40.44734712965495
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ]
+        
+        mapView.drawPolygon(geoJson: geoJson)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
