d80b3b541d1d29190dc625fb255e19d0a5566406
Jesus Victorio <jesus.victorio@geographica.gs>
2018-11-22 07:50:46 +0100

    temposdk

--- Example/Tempo-ios-sdk/AppDelegate.swift
+++ Example/Tempo-ios-sdk/AppDelegate.swift
@@ -12,15 +12,13 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    
-    var License = "XTUM0Q0ZRQ1JqaHpudExBaXI4MVRoeFYwdDF1bjNsU2xjd0lWQUtWOVlLTWFhRkwwTUYzWVJaNTJ1MzY2TWdtMAoKYXBwVG9rZW49OTQ4NDE5ODctODA5Mi00OTBlLTkwNDQtNzZlZWRkYWI3YmZhCmJ1bmRsZUlkZW50aWZpZXI9Z3MuZ2VvZ3JhcGhpY2EudGVtcG9zZGsKb25saW5lTGljZW5zZT0xCnByb2R1Y3RzPXNkay1pb3MtNC4qCndhdGVybWFyaz1jYXJ0b2RiCg==";
-    
-    
+   
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        NTLog.setShowInfo(true)
-        NTMapView.registerLicense(License)
         
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window!.rootViewController = UINavigationController.init(rootViewController: ViewController())
+        window!.makeKeyAndVisible()
         return true
     }
 }
